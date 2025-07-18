c081419c7c9a6c94463c477d428b3245e50326da
fatkquladhim <fatquladhim@gmail.com>
2025-07-18 14:23:46 +0700

    hapus foto

--- sql/tugas_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tugas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    judul VARCHAR(255) NOT NULL,
-    deskripsi TEXT NOT NULL,
-    deadline DATE NOT NULL,
-    id_penerima_tugas INT NOT NULL,
-    status ENUM('belum selesai', 'selesai') NOT NULL DEFAULT 'belum selesai',
-    FOREIGN KEY (id_penerima_tugas) REFERENCES users(id) ON DELETE CASCADE
-);
